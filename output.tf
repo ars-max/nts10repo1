f4611b701658a3ddab109589e148c5723cf4565d
arshad <arshad.kdlr@gmail.com>
2024-07-24 10:43:29 +0000

    c1

---
+++ output.tf
@@ -0,0 +1,13 @@
+output "vpc_id" {
+
+  value = aws_vpc.dep1.id
+
+}
+
+
+
+output "bkt_id" {
+
+  value = aws_s3_bucket.dep2.id
+
+}
---
+++ var.tf
@@ -0,0 +1,27 @@
+variable "var_cidr" {
+
+  type = string
+
+
+}
+
+
+
+variable "var_vpcname" {
+
+  type = string
+
+}
+
+
+
+variable "var_bktname" {
+
+  type = string
+
+}
+
+
+
+
+
